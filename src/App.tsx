e489d924dba0ecc80dff58033a2e6fae74f044ba
wswiselife <wswiselife@gmail.com>
2025-05-10 13:22:54 +0800

    feat: 项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+// App.tsx
+export default function App() {
+    return (
+        <h1 className="w-screen h-screen flex justify-center items-center text-pink-500 underline text-6xl">
+            Hello world!
+        </h1>
+    );
+}
